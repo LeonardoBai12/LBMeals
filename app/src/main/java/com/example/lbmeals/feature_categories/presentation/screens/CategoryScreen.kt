cf79c3002ecf8c7df9520a4261874eebdf50704d
Leonardo Biondo Bai <leonardobiondo21@gmail.com>
2022-07-20 22:45:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lbmeals/feature_categories/presentation/screens/CategoryScreen.kt
@@ -0,0 +1,9 @@
+package com.example.lbmeals.feature_categories.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun CategoryScreen(navController: NavHostController) {
+
+}
---
+++ app/src/main/java/com/example/lbmeals/feature_meal/presentation/screens/MealsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.lbmeals.feature_meal.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun MealsScreen(navController: NavHostController, category: String) {
+
+}
