f07c94647a46d5019ce1affe42b0c5071db0f3c6
Leonardo Biondo Bai <leonardobiondo21@gmail.com>
2022-07-23 14:21:43 -0300

    Adicionados nomes serializados que virão dos jsons

--- app/src/main/java/com/example/lbmeals/feature_categories/domain/model/Category.kt
+++ app/src/main/java/com/example/lbmeals/feature_categories/domain/model/Category.kt
@@ -1,9 +1,17 @@
 package com.example.lbmeals.feature_categories.domain.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Category(
+    @SerializedName("idMeal")
     val id: String,
+    @SerializedName("strCategory")
+    val category: String,
+    @SerializedName("strCategoryThumb")
+    val thumbnail: String,
+    @SerializedName("strCategoryDescription")
+    val description: String,
 ): Parcelable
