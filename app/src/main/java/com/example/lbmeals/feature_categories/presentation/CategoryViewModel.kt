724145c240660a6448c0bcae17e780bd7b520d38
Leonardo Biondo Bai <leonardobiondo21@gmail.com>
2022-07-29 20:50:28 -0300

    Corrigido erro de compilação

--- app/src/main/java/com/example/lbmeals/feature_categories/presentation/CategoryViewModel.kt
+++ app/src/main/java/com/example/lbmeals/feature_categories/presentation/CategoryViewModel.kt
@@ -2,16 +2,18 @@ package com.example.lbmeals.feature_categories.presentation
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
 import com.example.lbmeals.feature_categories.domain.model.Category
 import com.example.lbmeals.feature_categories.domain.use_case.CategoryUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import javax.inject.Inject
 
 @HiltViewModel
-class CategoryViewModel(
+class CategoryViewModel @Inject constructor(
     private val useCases: CategoryUseCases
-) {
+): ViewModel() {
     private val _state = mutableStateOf(CategoryState())
     val state: State<CategoryState> = _state
 
@@ -23,4 +25,6 @@ class CategoryViewModel(
         data class ShowToast(val message: String): UiEvent()
         object Finish: UiEvent()
     }
+
+
 }
