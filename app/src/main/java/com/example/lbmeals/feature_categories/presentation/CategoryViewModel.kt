45616712cd4fce2cd5d2f2786f0aa545f18225d9
Leonardo Biondo Bai <leonardobiondo21@gmail.com>
2022-07-29 01:49:11 -0300

    Adicionados eventos de UI

--- app/src/main/java/com/example/lbmeals/feature_categories/presentation/CategoryViewModel.kt
+++ app/src/main/java/com/example/lbmeals/feature_categories/presentation/CategoryViewModel.kt
@@ -2,8 +2,11 @@ package com.example.lbmeals.feature_categories.presentation
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import com.example.lbmeals.feature_categories.domain.model.Category
 import com.example.lbmeals.feature_categories.domain.use_case.CategoryUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
 @HiltViewModel
 class CategoryViewModel(
@@ -11,4 +14,13 @@ class CategoryViewModel(
 ) {
     private val _state = mutableStateOf(CategoryState())
     val state: State<CategoryState> = _state
+
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    sealed class UiEvent {
+        data class OnCategoryClicked(val category: Category): UiEvent()
+        data class ShowToast(val message: String): UiEvent()
+        object Finish: UiEvent()
+    }
 }
