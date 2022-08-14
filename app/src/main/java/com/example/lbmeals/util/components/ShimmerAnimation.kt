011d7817db6163e1af1355a2d4ce9f06b2269773
Leonardo Biondo Bai <leonardobiondo21@gmail.com>
2022-08-14 13:59:36 -0300

    Criada extensão genérica para uso de shimmer

---
+++ app/src/main/java/com/example/lbmeals/util/components/ShimmerAnimation.kt
@@ -0,0 +1,35 @@
+package com.example.lbmeals.util.components
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush.Companion.linearGradient
+import androidx.compose.ui.graphics.Color
+
+fun Modifier.shimmerAnimation(): Modifier = composed {
+    val colors = listOf(
+        Color.LightGray.copy(alpha = 0.9F),
+        Color.LightGray.copy(alpha = 0.2F),
+        Color.LightGray.copy(alpha = 0.9F),
+    )
+    val transition = rememberInfiniteTransition()
+    val translateAnimation = transition.animateFloat(
+        initialValue = 0F,
+        targetValue = 1000F,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing,
+            )
+        )
+    )
+    val brush = linearGradient(
+        colors,
+        start = Offset.Zero,
+        end = Offset(translateAnimation.value, translateAnimation.value)
+    )
+
+    background(brush)
+}
