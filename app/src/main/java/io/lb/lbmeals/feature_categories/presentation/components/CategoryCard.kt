4df4d889c1783b7420120a8f3dedfe942567083c
Leonardo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2023-03-05 01:26:07 -0300

    Correçao de code style

--- app/src/main/java/io/lb/lbmeals/feature_categories/presentation/components/CategoryCard.kt
+++ app/src/main/java/io/lb/lbmeals/feature_categories/presentation/components/CategoryCard.kt
@@ -11,13 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -82,7 +77,6 @@ fun CategoryCard(category: Category, onClick: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom
     ) {
-
         Image(
             modifier = Modifier.fillMaxSize(0.8F),
             painter = rememberAsyncImagePainter(
--- app/src/main/java/io/lb/lbmeals/feature_meals/presentation/listing/components/MealCard.kt
+++ app/src/main/java/io/lb/lbmeals/feature_meals/presentation/listing/components/MealCard.kt
@@ -9,13 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
